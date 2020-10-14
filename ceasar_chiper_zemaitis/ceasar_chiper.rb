fd4324a854798cadfcaadb57e4f9a2bcc428e0cd
JonasZ <zemaitis.jonas0@gmail.com>
2020-10-14 15:06:24 +0300

    rubocop corrected chiper project

--- ceasar_chiper_zemaitis/ceasar_chiper.rb
+++ ceasar_chiper_zemaitis/ceasar_chiper.rb
@@ -2,34 +2,30 @@
 
 # pagrindine shifravimo clase
 class CeasarChiper
-    def initialize(key)
-        @key = key
-    end
-    attr_reader :key
+  def initialize(key)
+    @key = key
+  end
+  attr_reader :key
 
-    def encrypt_str(input)
-        output = ''
-        input.split("").each do |i|
-            #puts " char: #{input[i].ord}"
-            val = i.ord + @key
-            if val > 127
-                val %= 127
-            end
-            output += val.chr
-        end
-        return output
+  # metodas skirtas stringo uzsifravimui
+  def encrypt_str(input)
+    output = ''
+    input.split('').each do |i|
+      val = i.ord + @key
+      val %= 127 if val > 127
+      output += val.chr
     end
+    output
+  end
 
-    def decrypt_str(input)
-        output = ''
-        input.split("").each do |i|
-            #puts " char: #{input[i].ord}"
-            val = i.ord - @key
-            if val > 127
-                val += 127
-            end
-            output += val.chr
-        end
-        return output
+  # metodas skirtas stringo desifravimui
+  def decrypt_str(input)
+    output = ''
+    input.split('').each do |i|
+      val = i.ord - @key
+      val += 127 if val > 127
+      output += val.chr
     end
+    output
+  end
 end
--- ceasar_chiper_zemaitis/main.rb
+++ ceasar_chiper_zemaitis/main.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
+# importinam sifravimo klase
 require_relative 'ceasar_chiper.rb'
 
+# inicijuojama sifravimo klase
 chiper = CeasarChiper.new(5)
 
-enc = chiper.encrypt_str "Labas 99 67"
+# uzsifruojame teksta
+enc = chiper.encrypt_str 'Labas 99 67'
+
+# atvaizduojame ekrane gautus rezultatus
 puts "Encrypted: #{enc}"
 puts "Decrypted: #{chiper.decrypt_str enc}"
