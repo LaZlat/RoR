49863a627ae49b07e107623d5ef0d04e1de4f58f
RytisRazmus <rytis.razmus@gmail.com>
2020-10-21 00:11:54 +0300

    fixed ruboop cries

--- polindromes_razmus/tests/test_plindromes.rb
+++ polindromes_razmus/tests/test_plindromes.rb
@@ -3,6 +3,7 @@
 require 'test/unit'
 require_relative '../polindromai.rb'
 
+# testuoja polindrimai.rb
 class TestAdd < Test::Unit::TestCase
   def test_form_polindrome
     polindromes = Polindromes.new
@@ -14,12 +15,13 @@ class TestAdd < Test::Unit::TestCase
     polindromes = Polindromes.new
     expected = 6
     expected_array = [11, 22, 33, 44, 55, 66]
-
     array = polindromes.calculate(expected)
     length = array.length
-
     assert_equal expected, length
+    test_values(array, expected_array)
+  end
 
+  def test_values(array, expected_array)
     i = 0
     array.each do |arr|
       assert_equal expected_array[i], arr
